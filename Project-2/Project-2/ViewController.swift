b5d9968a23d6c0c46e1ceef4a505b4fd98920a4d
Sagar Kadam <>
2022-06-17 10:40:43 +0530

    Code Update : Our current code chooses the first three items in the countries array, and places them into the three buttons on our view controller. This is fine to begin with, but really we need to choose random countries each time.

--- Project-2/Project-2/ViewController.swift
+++ Project-2/Project-2/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
    
     var countries = [String]()
     var score = 0
+    var correctAnswer = 0
     @IBAction func buttton1Clicked(_ sender: Any) {
     }
     
@@ -54,9 +55,12 @@ class ViewController: UIViewController {
     }
     
     func askQuestions(){
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        title = countries[correctAnswer].uppercased()
     }
 }
 
