6c5db3d3aae5d663dc8a1fe2c3de218c1d9c6da7
Sagar Kadam <>
2022-06-17 09:35:51 +0530

    Code Update : Added the asset files.

--- Project-2/Project-2/ViewController.swift
+++ Project-2/Project-2/ViewController.swift
@@ -8,7 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var button1: UIButton!
+    
+    @IBOutlet weak var button2: UIButton!
+    
+    @IBOutlet weak var button3: UIButton!
+    
+    
+    
+    @IBAction func buttton1Clicked(_ sender: Any) {
+    }
+    
+    @IBAction func buttton2Clicked(_ sender: Any) {
+    }
+    
+    @IBAction func buttton3Clicked(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
