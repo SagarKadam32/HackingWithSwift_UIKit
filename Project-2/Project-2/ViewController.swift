7159242c56156275b3c168f64b8a621c6fd21fd4
Sagar Kadam <>
2022-06-17 10:08:18 +0530

    Code Update : Making the basic game work: UIButton and CALayer

--- Project-2/Project-2/ViewController.swift
+++ Project-2/Project-2/ViewController.swift
@@ -10,13 +10,11 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var button1: UIButton!
-    
     @IBOutlet weak var button2: UIButton!
-    
     @IBOutlet weak var button3: UIButton!
-    
-    
-    
+   
+    var countries = [String]()
+    var score = 0
     @IBAction func buttton1Clicked(_ sender: Any) {
     }
     
@@ -28,9 +26,37 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        /*
+        countries.append("estonia")
+        countries.append("france")
+        countries.append("germany")
+        countries.append("ireland")
+        countries.append("italy")
+        countries.append("monaco")
+        countries.append("nigeria")
+        countries.append("poland")
+        countries.append("russia")
+        countries.append("spain")
+        countries.append("uk")
+        countries.append("us") */
+        
+        countries += ["estonia","france","germany","ireland","italy","monaco","nigeria","poland","russia","spain","uk","us"]
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        askQuestions()
     }
-
-
+    
+    func askQuestions(){
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
 }
 
