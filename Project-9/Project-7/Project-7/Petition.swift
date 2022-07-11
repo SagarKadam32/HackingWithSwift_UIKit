6a7689bebb8feca3a7566fd0497f53de3328511c
Sagar Kadam <>
2022-07-11 22:00:16 +0530

    Project 9: Grand Central Dispatch Description: Learn how to run complex tasks in the background with GCD.

---
+++ Project-9/Project-7/Project-7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project-7
+//
+//  Created by Sagar Kadam on 10/07/22.
+//
+
+import Foundation
+
+struct Petition : Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project-9/Project-7/Project-7/ViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ViewController.swift
+//  Project-7
+//
+//  Created by Sagar Kadam on 08/07/22.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var petitions = [Petition]()
+    var filteredPetitions = [Petition]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let urlString : String
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(searchForPetition))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "About", style: .plain, target: self, action: #selector(aboutInfoButtonClicked))
+        
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        }else {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
+        
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url){
+                // we're OK to parse
+                parse(json: data)
+                return
+            }
+        }
+        showError()
+    }
+    
+    func showError() {
+        let ac = UIAlertController(title: "Laoding Error", message: "There was a problem loading the feed; pleae check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+        
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            filteredPetitions = petitions
+            tableView.reloadData()
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredPetitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let petition = filteredPetitions[indexPath.row]
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = filteredPetitions[indexPath.row]
+        filteredPetitions.append(filteredPetitions[indexPath.row])
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @objc func aboutInfoButtonClicked() {
+        let ac = UIAlertController(title: "About US", message: "This data comes from the We The People API of the Whitehouse", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    @objc func searchForPetition() {
+        let ac = UIAlertController(title: "Search", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let searchAction = UIAlertAction(title: "Search", style: .default) { [weak self, weak ac]  action in
+            guard let searchString = ac?.textFields?[0].text else {return}
+            self?.searchPetitionText(searchString)
+        }
+        
+        ac.addAction(searchAction)
+        present(ac, animated: true)
+    }
+    
+    func searchPetitionText(_ searchString: String) {
+        filteredPetitions.removeAll()
+        for petition in petitions {
+        
+            if(petition.title.contains(searchString) || petition.body.contains(searchString)){
+                filteredPetitions.append(petition)
+            }
+        }
+        tableView.reloadData()
+        
+    }
+}
+                                                                                                                                                                                                                    
