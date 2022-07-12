dde503de883819b05eff0c1b917af52a703bc060
Sagar Kadam <>
2022-07-12 09:54:54 +0530

    Project 10: Names to Faces : Initial Files

---
+++ Project-10/Project-10/Project-10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project-10
+//
+//  Created by Sagar Kadam on 12/07/22.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
