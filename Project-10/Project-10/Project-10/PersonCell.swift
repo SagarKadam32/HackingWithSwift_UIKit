8e5561b5fda207b3e8e9a886ac1427ad2da9728f
Sagar Kadam <>
2022-07-12 10:02:32 +0530

    Designing UICollectionView cells
    https://www.hackingwithswift.com/read/10/2/designing-uicollectionview-cells

---
+++ Project-10/Project-10/Project-10/PersonCell.swift
@@ -0,0 +1,17 @@
+//
+//  PersonCell.swift
+//  Project-10
+//
+//  Created by Sagar Kadam on 12/07/22.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    
+    
+    
+}
