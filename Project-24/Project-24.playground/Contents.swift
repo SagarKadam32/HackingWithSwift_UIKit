32fb417f4fe336d330b6995284d119a023a10277
Sagar Kadam <>
2022-07-17 16:50:26 +0530

    Strings are not arrays https://www.hackingwithswift.com/read/24/2/strings-are-not-arrays

--- Project-24/Project-24.playground/Contents.swift
+++ Project-24/Project-24.playground/Contents.swift
@@ -1,3 +1,21 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+let name = "Taylor"
+
+
+for letter in name {
+    print("Give me a \(letter)!")
+}
+          
+          let letter = name[name.index(name.startIndex, offsetBy: 3)]
+
+extension String {
+    subscript(i: Int) -> String {
+        return String(self[index(startIndex, offsetBy: i)])
+    }
+}
+
+
+name[3]
