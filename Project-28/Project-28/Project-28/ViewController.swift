86d158e14700b50b9ccc2dda396c51bc6f7c853d
Sagar Kadam <>
2022-07-17 19:42:03 +0530

    Creating a basic text editor https://www.hackingwithswift.com/read/28/2/creating-a-basic-text-editor

---
+++ Project-28/Project-28/Project-28/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  Project-28
+//
+//  Created by Sagar Kadam on 17/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var secret: UITextView!
+    
+    @IBOutlet weak var authenticateButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+
+    }
+
+    
+    @IBAction func authenticateClicked(_ sender: Any) {
+        
+        
+    }
+    
+    @objc func adjustForKeyboard(notification: Notification) {
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            secret.contentInset = .zero
+        } else {
+            secret.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+        }
+
+        secret.scrollIndicatorInsets = secret.contentInset
+
+        let selectedRange = secret.selectedRange
+        secret.scrollRangeToVisible(selectedRange)
+    }
+}
+
