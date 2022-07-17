d7754af04ec52c9ea1d06f1f56dc72656578bd78
Sagar Kadam <>
2022-07-17 17:35:04 +0530

    Working with strings in Swift https://www.hackingwithswift.com/read/24/3/working-with-strings-in-swift

--- Project-24/Project-24.playground/Contents.swift
+++ Project-24/Project-24.playground/Contents.swift
@@ -19,3 +19,57 @@ extension String {
 
 
 name[3]
+
+let password = "123456"
+password.hasPrefix("123")
+password.hasSuffix("456")
+
+extension String {
+    func deletingPrefix(_ prefix: String) -> String {
+        guard self.hasPrefix(prefix) else { return self }
+        return String(self.dropFirst(prefix.count))
+    }
+    
+    func deletingSuffix(_ suffix: String) -> String {
+        guard self.hasSuffix(suffix) else { return self}
+        return String(self.dropLast(suffix.count))
+    }
+}
+
+password.deletingPrefix("12")
+password.deletingSuffix("12")
+
+let weather = "it's going to rain"
+print(weather.capitalized)
+
+extension String {
+    var capitalizedFirst: String {
+        guard let firstLetter = self.first else { return "" }
+        return firstLetter.uppercased() + self.dropFirst()
+    }
+}
+
+print(weather.capitalizedFirst)
+
+let input = "Swift is like Objective-C without the C"
+input.contains("Swift")
+
+let languages = ["Python", "Ruby", "Swift"]
+languages.contains("Swift")
+
+extension String {
+    func containsAny(of array: [String]) -> Bool {
+        for item in array {
+            if self.contains(item) {
+                return true
+            }
+        }
+
+        return false
+    }
+}
+
+input.containsAny(of: languages)
+
+languages.contains(where: input.contains)
+
