cfb820abc0ae549abde9bed0e82e057d2468fe8b
Sagar Kadam <>
2022-09-02 16:32:53 +0530

    Adding user locations to a map https://www.hackingwithswift.com/books/ios-swiftui/adding-user-locations-to-a-map

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+
+var number1 = 1.0
+var number2 = 2.0
+
+var total = number1 + number2
+
+
+// SET
+let colors = Set(["Red", "Green", "Blue"])
+let colors1 = Set(["Red", "Red"])
+
+// TUPLES
+var name = (first: "Sagar", second: "Kadam")
+name.0
+name.first
+name.first = "Bond"
+name.0
+
+//Arrays vs sets vs tuples
+let address = (house:502, street: "Tejpal Scheme 2", location: "Vileparle")
+let mySet = Set(["Mahant Road", "Sant Janabai Road", "Subhash Road"])
+let myArray = ["Run-1", "Run-2", "Run-2"]
+
